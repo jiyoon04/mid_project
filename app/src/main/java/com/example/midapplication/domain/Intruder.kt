2b97f2f18b7a5a4c82d13474cf4a346c9c7d15eb
jiyoon04 <94222592+jiyoon04@users.noreply.github.com>
2023-11-07 18:16:41 +0900

    git commit -m "first commit"

---
+++ app/src/main/java/com/example/midapplication/domain/Intruder.kt
@@ -0,0 +1,9 @@
+package com.example.midapplication.domain
+
+data class intruder(
+    val created_date: String,
+    val image: String,
+    val published_date: String,
+    val text: String,
+    val title: String
+)
